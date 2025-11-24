03be1e53c2bc3dd636f00fb137919cf6bef089ee
ManuelDiLena <manuel.dilena.29@gmail.com>
2025-11-24 20:16:52 +0100

    React router config

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,27 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ProtectedRoute from './components/ProtectedRoute';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import Onboarding from './pages/Onboarding';
+import Dashboard from './pages/Dashboard';
+
 function App() {
   return (
-    <div className='flex h-screen w-full items-center justify-center bg-gray-900'>
-      <h1 className='text-3xl font-bold text-green-500'>
-        Futbol App - Frontend
-      </h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        {/* PUBLIC ROUTES */}
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        {/* PROTECTED ROUTES */}
+        <Route element={<ProtectedRoute />}>
+          <Route path='/dashboard' element={<Dashboard />} />
+          <Route path='/onboarding' element={<Onboarding />} />
+        </Route>
+        {/* DEFAULT ROUTES */}
+        <Route path='/' element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
